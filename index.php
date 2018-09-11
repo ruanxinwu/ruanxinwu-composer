50199ab1b37001eab36c084d5cb890a79d208129
ruanxinwu <770597562@qq.com>
2018-09-11 15:49:20 +0800

    s

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-aduiadhasfiqwehfu
+<?php
+namespace yii\dadaadjckanj;
+use Pheanstalk\Pheanstalk;
+
+require './vendor/autoload.php';
+function pd(){
+    echo '<pre>'.PHP_EOL;
+    for($i=0;$i<func_num_args();$i++){
+        echo '---------------'.($i+1).'---------------'.PHP_EOL;
+        print_r(func_get_arg($i));
+        echo PHP_EOL;
+    }
+    echo '</pre>';
+    die;
+}
+//BEANSTALKD_HOST=123.207.164.102
+//BEANSTALKD_PORT=11300
+$pheanstalk = new Pheanstalk('123.207.164.102');
+//$pheanstalk->useTube('abc')->put(654321);die;
+$job = $pheanstalk->watch('abc')->ignore('default')->reserve();
+echo 23;
+echo $job->getData(),$job->getId();//die;
+$pheanstalk->delete($job);
+
